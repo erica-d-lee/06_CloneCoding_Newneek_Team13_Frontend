2671359515087a3138527dc3f42af92c292ac6ed
leedmeen <leedmeen@gmail.com>
2021-07-16 20:41:56 +0900

    텍스트 element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Menu from './elements/Menu';
+import Text from './elements/Text';
 
 function App() {
   return (
     <div className="App">
       안녕하세요 newneek입니다!!
-      <Menu></Menu>
+      <Text></Text>
     </div>
   );
 }
--- src/elements/Text.js
+++ src/elements/Text.js
@@ -2,12 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Text = (props) => {
-
+  const { value, mainText, mainTitle, detailText } = props;
+  const styles = { mainText: mainText, mainTitle: mainTitle, detailText: detailText, };
   return (
     <React.Fragment>
-      <div></div>
+      <P {...styles}>{value}</P>
     </React.Fragment>
   )
 };
+
+Text.defaultProps = {
+  value: '텍스트',
+  mainText: false,
+  mainTitle: false,
+  detailText: false,
+};
+
+const P = styled.p`
+  ${(props) => props.mainText ? 'font-size: 1rem': ''};
+  ${(props) => props.mainTitle ? 'font-size: 1.5rem': ''};
+  ${(props) => props.detailText ? 'font-size: 1.125rem': ''};
+`;
 
 export default Text;
