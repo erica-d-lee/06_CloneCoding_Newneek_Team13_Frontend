552f35307fe55e001430e3dc81a77e0d7ecc955b
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2021-07-16 20:53:32 +0900

    firstpull

--- src/elements/index.js
+++ src/elements/index.js
@@ -1,9 +1,8 @@
 import Grid from "./Grid";
 import Image from "./Image";
-import Text from "./Text";
 import Input from "./Input";
 import Button from "./Button";
 
 
 
-export { Grid, Image, Text, Input, Button, };
+export { Grid, Image, Input, Button, };
